52db04e69e47ac24819aae06a87350bf8b38ba13
ksp0814 <rkdtpvlf456@naver.com>
2025-06-14 23:45:30 +0900

    첫 번째 커밋

---
+++ src/test/java/com/example/Kang/KangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Kang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
